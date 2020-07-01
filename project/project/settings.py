c5d9db5300a95f7efbdfb3037a04bf0b995d7c53
luoschenfeng <luoschenfeng@gmail.com>
2020-07-01 15:36:01 +0800

    替换后端模板引擎为Jinja2

--- project/project/settings.py
+++ project/project/settings.py
@@ -56,6 +56,19 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+    {
+        'BACKEND': 'django.template.backends.jinja2.Jinja2',
         'DIRS': [os.path.join(BASE_DIR, 'frontend/dist/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -65,6 +78,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'environment': 'project.jinja2.environment',
         },
     },
 ]
